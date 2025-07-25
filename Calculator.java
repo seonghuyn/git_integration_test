2c3c0f0c92bf41c88f9137f388f11fc0a5b90759
seonghuyn <busterkim07@naver.com>
2025-07-25 10:36:23 +0900

    20

---
+++ Calculator.java
@@ -0,0 +1,11 @@
+// 작업2
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("1+1=2");
+        System.out.println("1-1=0");
+        System.out.println("2*4=8");
+        System.out.println("4/2=2");
+        System.out.println("8/2=4");
+        System.out.println("10*2=20");
+    }
+}
