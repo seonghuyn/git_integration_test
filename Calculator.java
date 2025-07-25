b65bc67b4505d9e4ad579bee665c00078b6207f8
seonghuyn <busterkim07@naver.com>
2025-07-25 10:22:19 +0900

    20

--- Calculator.java
+++ Calculator.java
@@ -5,5 +5,6 @@ public class Calculator {
         System.out.println("2*4=8");
         System.out.println("4/2=2");
         System.out.println("8/2=4");
+        System.out.println("10*2=20");
     }
 }
